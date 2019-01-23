b72b0664046310c34338ff084569237d3c6dd4ee
Raymond Nie <rnie@nvidia.com>
2019-01-23 11:54:44 -0800

    cc

--- example_configs/text2speech/deepvoice3_fp32.py
+++ example_configs/text2speech/deepvoice3_fp32.py
@@ -9,54 +9,82 @@ from open_seq2seq.optimizers.lr_policies import fixed_lr, transformer_policy, ex
 base_model = DeepVoice
 dataset = "LJ"
 dataset_location = "/home/rnie/Desktop/rnie/dataset/LJSpeech_mixed"
-# dataset_location = "/data/LJSpeech"
 output_type = "mel"
-data_min = 1e-2
-trim = False
+
+if dataset == "MAILABS":
+  trim = True
+  mag_num_feats = 401
+  train = "train.csv"
+  val = "val.csv"
+  batch_size = 32
+elif dataset == "LJ":
+  trim = False
+  mag_num_feats = 513
+  train = "train_32.csv"
+  val = "val_32.csv"
+  batch_size = 48
+else:
+  raise ValueError("Unknown dataset")
+
 exp_mag = False
-num_audio_features = 80
+if output_type == "magnitude":
+  num_audio_features = mag_num_feats
+  data_min = 1e-5
+elif output_type == "mel":
+  num_audio_features = 80
+  data_min = 1e-2
+elif output_type == "both":
+  num_audio_features = {
+      "mel": 80,
+      "magnitude": mag_num_feats
+  }
+  data_min = {
+      "mel": 1e-2,
+      "magnitude": 1e-5,
+  }
+  exp_mag = False
+else:
+  raise ValueError("Unknown param for output_type")
+
 '''
 batch_size == B
 emb_size == e
 encoder_channels == c
 reduction_factor == r
 '''
-reduction_factor = None
-keep_prob = 0.9
+reduction_factor = 4
+keep_prob = 0.95
 base_params = {
   "use_horovod": False,
   "num_gpus": 1,
   # "logdir": "/results/deepvoice3_fp32",
-  "logdir": "/home/rnie/Desktop/rnie/OpenSeq2Seq/will_it_run",
-  "save_summaries_steps": 500,
-  "print_loss_steps": 500,
-  "print_samples_steps": 500,
+  "logdir": "/home/rnie/Desktop/rnie/OpenSeq2Seq/will_it_run4",
+  "save_summaries_steps": 100,
+  "print_loss_steps": 100,
+  "print_samples_steps": 100,
   "save_checkpoint_steps": 500,
-  "save_to_tensorboard": False,
+  "save_to_tensorboard": True,
   "regularizer": tf.contrib.layers.l2_regularizer,
   "regularizer_params": {
     'scale': 1e-6
   },
   "optimizer": "Adam",
-  "optimizer_params": {},
-  "lr_policy": exp_decay,
+  "optimizer_params": {
+  },
+  "lr_policy": fixed_lr,
   "lr_policy_params": {
-    "learning_rate": 1e-3,
-    "decay_steps": 10000,
-    "decay_rate": 0.1,
-    "use_staircase_decay": False,
-    "begin_decay_at": 20000,
-    "min_lr": 1e-5,
+    "learning_rate": 0.0001
   },
   "summaries": ["learning_rate", "gradients", "gradient_norm", "global_gradient_norm"],  
-  "batch_size_per_gpu": 64,
+  "batch_size_per_gpu": 16,
   "max_steps": 200000,
   "dtype": tf.float32,
   "max_grad_norm":1.,
+  "reduction_factor": reduction_factor,
   "data_layer": Text2SpeechDataLayer,
   "data_layer_params": {
     "dataset_files": [
-      os.path.join(dataset_location, "test.csv"),
+      os.path.join(dataset_location, "train.csv"),
     ],
     "dataset": dataset,
     "num_audio_features": num_audio_features,
@@ -99,8 +127,10 @@ base_params = {
       "decoder_layers": 4,
       "kernel_size": 5,
       "keep_prob": keep_prob,
-      "reduction_factor": reduction_factor
   },
   # Loss params
-  "loss": DeepVoiceLoss
+  "loss": DeepVoiceLoss,
+  "loss_params": {
+    "l1_loss": False
+  }
 }
