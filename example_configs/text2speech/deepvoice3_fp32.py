979b8ea105b4e9dbd125f38b745dbaea01ceb35d
Raymond Nie <rnie@nvidia.com>
2019-02-20 15:32:34 -0800

    horovod off for single gpu

--- example_configs/text2speech/deepvoice3_fp32.py
+++ example_configs/text2speech/deepvoice3_fp32.py
@@ -58,8 +58,8 @@ reduction_factor == r
 reduction_factor = 4
 keep_prob = 0.95
 base_params = {
-  "use_horovod": True,
-  # "num_gpus": 1,
+  "use_horovod": False,
+  "num_gpus": 1,
   # "logdir": "/home/rnie/Desktop/rnie/OpenSeq2Seq/wn3",
   "logdir": "/results/deepvoice3_fp32",
   "save_summaries_steps": 100,
